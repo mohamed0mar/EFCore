0f5645d129302e83b798dc8002980204b0f99af6
Mohamed Mahmoud <Mohamed_20180534@fci.helwan.edu.eg>
2025-08-18 13:11:21 +0300

    -Entity Framework Core

--- EFCore/Program.cs
+++ EFCore/Program.cs
@@ -4,6 +4,24 @@
     {
         static void Main(string[] args)
         {
+            #region What Is ORM
+            // Entity Framework Core is a popular Object-Relational Mapper (ORM) for .NET applications.
+            // ORM Is Tools To Help Me to Intract With Database Easily.
+            // Object-Relational Mapping (ORM) is a programming technique for converting data between incompatible
+            // type systems in object-oriented programming languages.
+            // ORM allows developers to work with databases using objects,
+            // ORM DO Two Things:
+            // 1. Mapping =>
+            // 1.1.From Code To Database Table =>Code First [Must Commen]
+            // 1.2.From Database Table To Code =>Database First
+            // 2. L2EF [LINQ To Entity FrameWork]=>
+            // Query Object Models Using LINQ [CRUD]
+            // Generation Will Follow Wchich Schema 
+            /// 1. Table Per Type (TPT)
+            /// 2. Table Per Hierarchy (TPH)
+            /// 3. Table Per Concrete Class (TPC) 
+            #endregion
+
 
         }
     }
